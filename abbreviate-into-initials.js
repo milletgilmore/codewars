4967dd3a177d950f8421837061dae42ee9fe9c84
milletgilmore <milletgilmore@gmail.com>
2022-07-13 17:22:45 -0500

    adding an alternate solution

--- abbreviate-into-initials.js
+++ abbreviate-into-initials.js
@@ -12,3 +12,9 @@ function abbrevName(name){
     return name.split(' ').map(i => i[0].toUpperCase()).join('.')
 
 }
+
+function abbrevName(name){
+
+    var nameArray = name.split(" ");
+    return (nameArray[0][0] + "." + nameArray[1][0]).toUpperCase();
+  }
