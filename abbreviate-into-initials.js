6447d9a51e9f3134d50c758163994388dc1202aa
milletgilmore <milletgilmore@gmail.com>
2022-07-19 08:56:21 -0500

    Take in a name and return their initials

---
+++ abbreviate-into-initials.js
@@ -0,0 +1,14 @@
+//Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+//The output should be two capital letters with a dot separating them.
+
+
+//first split the string where the space is so split(' ')
+//next create a new string with map and only pass in the first letter of the two strings
+//then make them upperCase
+//finally join the two strings with a '.' in the middle
+
+function abbrevName(name){
+
+    return name.split(' ').map(i => i[0].toUpperCase()).join('.')
+
+}
