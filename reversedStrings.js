fea5f17a15ab0cfd68883e6c865e879144723062
milletgilmore <milletgilmore@gmail.com>
2022-07-25 08:48:04 -0500

    new codewars

---
+++ reversedStrings.js
@@ -0,0 +1,2 @@
+//Complete the solution so that it reverses the string passed into it.
+let solution = str => str.split('').reverse().join('')
---
+++ squareNSum.js
@@ -0,0 +1,6 @@
+//Complete the square sum function so that it squares each number passed into it and then sums the results together.
+function squareSum(numbers){
+    let product =0
+    numbers.forEach(n => {product += n*n})
+      return product 
+    }
