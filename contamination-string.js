9df093ce0501e1d14f5dd4e9fcd70acc7558d8cf
milletgilmore <milletgilmore@gmail.com>
2022-08-17 08:36:33 -0500

    replacing a string with another string

---
+++ contamination-string.js
@@ -0,0 +1,5 @@
+//Both parameters are a string. Return the first parameter but replace each character with the value in parameter 2.
+
+function contamination(text, char){
+    return char.repeat(text.length)
+  }
